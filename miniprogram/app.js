66c7bd7255e91c4f56570f1310e466bdb91f3209
zlm <zlm>
2018-11-15 10:14:30 +0800

    111

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,5 +1,19 @@
 //app.js
 App({
+  data: {
+    test: 'zlm'
+  },
+  globalData: {
+    appid: '',
+    secret: ''
+  },
+
   onLaunch: function () {
+    var that = this
+    var user = wx.getStorageSync('user') || {}; 
+    var userInfo = wx.getStorageSync('userInfo') || {}; 
+  
+
+
   }
 })
