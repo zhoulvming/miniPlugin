8c2c45ccff0328db687b9e157bba789b5836484c
zlm <zlm>
2018-11-28 11:56:05 +0800

    111

--- code/plugin/pages/detail/index.js
+++ code/plugin/pages/detail/index.js
@@ -1,6 +1,8 @@
 // plugin/pages/detail/index.js
 Page({
-  data: {},
+  data: {
+    details_content: "凭券可到店兑换1份炭烤鸡腿帕尼尼，仅限1次兑换。\n\n1.使用有效期：自购买日起14天内使用有效，仅限6:00~9:00使用。\n2.这里是使用规则2说明部分"
+  },
   onLoad: function () {
     console.log('This is a detail page!');
   }
--- code/plugin/pages/index-page.js
+++ code/plugin/pages/index-page.js
@@ -1,12 +1,41 @@
 // plugin/pages/index-page.js
 Page({
-  data: {},
+  data: {
+    imgUrls: [
+      '../images/pt002.jpg',
+      '../images/pt001-detail.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
   onLoad: function () {
     console.log('This is a plugin home page!');
   },
   method: {
     gotoList: function() {
       console.log('will goto ....')
+    },
+    changeIndicatorDots: function (e) {
+      this.setData({
+        indicatorDots: !this.data.indicatorDots
+      })
+    },
+    changeAutoplay: function (e) {
+      this.setData({
+        autoplay: !this.data.autoplay
+      })
+    },
+    intervalChange: function (e) {
+      this.setData({
+        interval: e.detail.value
+      })
+    },
+    durationChange: function (e) {
+      this.setData({
+        duration: e.detail.value
+      })
     }
   }
 });
